3e092f2064cda35898e6587fba36061c8dbce06d
Hasan abbas Tharadara <tharadarahasan7860@gmail.com>
2025-05-26 16:31:49 +0530

    background Color changer

---
+++ bg_color_btn/postcss.config.js
@@ -0,0 +1,14 @@
+// export default {
+//   plugins: [
+//     require('autoprefixer')
+//   ]
+// }
+
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').Config} */
+export default {
+  plugins: [
+    autoprefixer
+  ]
+};
